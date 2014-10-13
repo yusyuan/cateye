a88bbf8ac715ca5ec449ca2aad57f454c118997a
yusyuan <yusyuan@program.com.tw>
2014-10-13 10:22:05 +0800

    刪除練習網頁

--- black.php
+++
@@ -1,9 +0,0 @@
-<img src="black.php" />
-<?php
-header("Content-Type: image/png");
-$image = imagecreate(200,200);
-$white = imagecolorallocate($image, 0xFF, 0xFF, 0xFF);
-$black = imagecolorallocate($image, 0x00, 0x00, 0x00);
-imagefilledrectangle($imaage, 50, 50, 150, 150, $black);
-imagepng($image);
-?>
